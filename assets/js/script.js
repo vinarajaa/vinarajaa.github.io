b4088c5970ce474c67e21a5474d31a01d5e7ae7c
vinarajaa <106210197+vinarajaa@users.noreply.github.com>
2025-02-18 18:06:58 -0800

    Some more changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,4 +5,13 @@ document.addEventListener("DOMContentLoaded", function () {
         backSpeed: 30,
         loop: true
     });
+
+    particlesJS("particles-js", {
+        particles: {
+            number: { value: 100 },
+            size: { value: 3 },
+            move: { speed: 1 },
+            line_linked: { enable: true, color: "#64ffda" }
+        }
+    });
 });
