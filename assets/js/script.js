327da249080ad9f8a7247af91c58f50764b41a52
vinarajaa <106210197+vinarajaa@users.noreply.github.com>
2025-02-18 18:00:44 -0800

    Initial portfolio setup

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let typed = new Typed("#typed-text", {
+        strings: ["a Web Developer", "a Tech Enthusiast", "a Problem Solver"],
+        typeSpeed: 50,
+        backSpeed: 30,
+        loop: true
+    });
+});
