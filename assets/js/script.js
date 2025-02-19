c85552ad380bd7d24af709a4b14d25acb0e1cc59
vinarajaa <106210197+vinarajaa@users.noreply.github.com>
2025-02-18 21:52:39 -0800

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     let typed = new Typed("#typed-text", {
-        strings: ["a Web Developer", "a Tech Enthusiast", "a Problem Solver"],
+        strings: ["An Aspiring AI Engineer", "a Creative", "a Problem Solver", 'Tech Enthusiast'],
         typeSpeed: 50,
         backSpeed: 30,
         loop: true
