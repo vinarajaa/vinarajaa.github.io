5269e23b5c3239cae30466f60124bc51ed05e602
vinarajaa <106210197+vinarajaa@users.noreply.github.com>
2025-02-18 22:47:05 -0800

    new background

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
     let typed = new Typed("#typed-text", {
-        strings: ["An Aspiring AI Engineer", "a Creative", "a Problem Solver", 'Tech Enthusiast'],
+        strings: ["a Web Developer", "a Tech Enthusiast", "a Problem Solver"],
         typeSpeed: 50,
         backSpeed: 30,
         loop: true
     });
 
+    VANTA.WAVES({
+        el: "#vanta-bg",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0x1a0120 // Adjust this color as needed
+    });
 });
 const particleVertex = `
   attribute float scale;
