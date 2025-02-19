12276e8096e3f7dc7df80c3744f47f34b1150259
vinarajaa <106210197+vinarajaa@users.noreply.github.com>
2025-02-18 22:52:34 -0800

    move vanta

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,128 +36,128 @@ const particleVertex = `
 	}
 `;
 
-const particleFragment = `
-	void main() {
-		gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-	}
-`;
+// const particleFragment = `
+// 	void main() {
+// 		gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+// 	}
+// `;
 
-function lerp(start, end, amount) {
-	return (1 - amount) * start + amount * end;
-};
+// function lerp(start, end, amount) {
+// 	return (1 - amount) * start + amount * end;
+// };
 
-class Canvas {
-	constructor() {
-		this.config = {
-			canvas: document.querySelector('#c'),
-			winWidth: window.innerWidth,
-			winHeight: window.innerHeight,
-			aspectRatio: window.innerWidth / window.innerHeight,
-			mouse: new THREE.Vector2(-10, -10)
-		};
+// class Canvas {
+// 	constructor() {
+// 		this.config = {
+// 			canvas: document.querySelector('#c'),
+// 			winWidth: window.innerWidth,
+// 			winHeight: window.innerHeight,
+// 			aspectRatio: window.innerWidth / window.innerHeight,
+// 			mouse: new THREE.Vector2(-10, -10)
+// 		};
 
-		this.onResize = this.onResize.bind(this);
-    this.onMouseMove = this.onMouseMove.bind(this);
-		this.animate = this.animate.bind(this);
+// 		this.onResize = this.onResize.bind(this);
+//     this.onMouseMove = this.onMouseMove.bind(this);
+// 		this.animate = this.animate.bind(this);
 
-		this.initCamera();
-		this.initScene();
-		this.initRenderer();
+// 		this.initCamera();
+// 		this.initScene();
+// 		this.initRenderer();
 
-		this.initParticles();
+// 		this.initParticles();
 
-		this.bindEvents();
-		this.animate();
-	}
+// 		this.bindEvents();
+// 		this.animate();
+// 	}
 
-	bindEvents() {
-		window.addEventListener('resize', this.onResize);
-		window.addEventListener('mousemove', this.onMouseMove, false);
-	}
+// 	bindEvents() {
+// 		window.addEventListener('resize', this.onResize);
+// 		window.addEventListener('mousemove', this.onMouseMove, false);
+// 	}
 
-	initCamera() {
-		this.camera = new THREE.PerspectiveCamera(75, this.config.aspectRatio, 0.01, 1000);
-		this.camera.position.set(0, 6, 5);
-	}
+// 	initCamera() {
+// 		this.camera = new THREE.PerspectiveCamera(75, this.config.aspectRatio, 0.01, 1000);
+// 		this.camera.position.set(0, 6, 5);
+// 	}
 
-	initControls() {
-		this.controls = new OrbitControls(this.camera, this.config.canvas);
-	}
+// 	initControls() {
+// 		this.controls = new OrbitControls(this.camera, this.config.canvas);
+// 	}
 
-	initScene() {
-		this.scene = new THREE.Scene();
-	}
+// 	initScene() {
+// 		this.scene = new THREE.Scene();
+// 	}
 
-	initRenderer() {
-		this.renderer = new THREE.WebGLRenderer({
-			canvas: this.config.canvas,
-			antialias: true,
-      // alpha: true
-		});
-		this.renderer.setPixelRatio(window.devicePixelRatio);
-		this.renderer.setSize(this.config.winWidth, this.config.winHeight);
-	}
+// 	initRenderer() {
+// 		this.renderer = new THREE.WebGLRenderer({
+// 			canvas: this.config.canvas,
+// 			antialias: true,
+//       // alpha: true
+// 		});
+// 		this.renderer.setPixelRatio(window.devicePixelRatio);
+// 		this.renderer.setSize(this.config.winWidth, this.config.winHeight);
+// 	}
 
-	initParticles() {
-    const gap = 0.5;
-    const amountX = 100;
-    const amountY = 200;
-    const particleNum = amountX * amountY;
-		const particlePositions = new Float32Array(particleNum * 3);
-    const particleScales = new Float32Array(particleNum);
-    let i = 0;
-    let j = 0;
+// 	initParticles() {
+//     const gap = 0.5;
+//     const amountX = 100;
+//     const amountY = 200;
+//     const particleNum = amountX * amountY;
+// 		const particlePositions = new Float32Array(particleNum * 3);
+//     const particleScales = new Float32Array(particleNum);
+//     let i = 0;
+//     let j = 0;
 
-    for (let ix = 0; ix < amountX; ix++) {
-      for (let iy = 0; iy < amountY; iy++) {
-        particlePositions[i] = ix * gap - ((amountX * gap) / 2);
-        particlePositions[i + 1] = 0;
-        particlePositions[i + 2] = iy * gap - ((amountX * gap) / 2);
-        particleScales[j] = 1;
-        i += 3;
-        j++;
-      }
-    }
+//     for (let ix = 0; ix < amountX; ix++) {
+//       for (let iy = 0; iy < amountY; iy++) {
+//         particlePositions[i] = ix * gap - ((amountX * gap) / 2);
+//         particlePositions[i + 1] = 0;
+//         particlePositions[i + 2] = iy * gap - ((amountX * gap) / 2);
+//         particleScales[j] = 1;
+//         i += 3;
+//         j++;
+//       }
+//     }
 
-		this.particleGeometry = new THREE.BufferGeometry();
-		this.particleGeometry.setAttribute('position', new THREE.BufferAttribute(particlePositions, 3));
-    this.particleGeometry.setAttribute('scale', new THREE.BufferAttribute(particleScales, 1));
+// 		this.particleGeometry = new THREE.BufferGeometry();
+// 		this.particleGeometry.setAttribute('position', new THREE.BufferAttribute(particlePositions, 3));
+//     this.particleGeometry.setAttribute('scale', new THREE.BufferAttribute(particleScales, 1));
 
-		this.particleMaterial = new THREE.ShaderMaterial({
-			transparent: true,
-			vertexShader: particleVertex,
-			fragmentShader: particleFragment,
-			uniforms: {
-				uTime: { type: 'f', value: 0 }
-			}
-		});
-		this.particles = new THREE.Points(this.particleGeometry, this.particleMaterial);
-		this.scene.add(this.particles);
-	}
+// 		this.particleMaterial = new THREE.ShaderMaterial({
+// 			transparent: true,
+// 			vertexShader: particleVertex,
+// 			fragmentShader: particleFragment,
+// 			uniforms: {
+// 				uTime: { type: 'f', value: 0 }
+// 			}
+// 		});
+// 		this.particles = new THREE.Points(this.particleGeometry, this.particleMaterial);
+// 		this.scene.add(this.particles);
+// 	}
 
-	render() {
-		this.camera.lookAt(this.scene.position);
-		this.renderer.render(this.scene, this.camera);
-	}
+// 	render() {
+// 		this.camera.lookAt(this.scene.position);
+// 		this.renderer.render(this.scene, this.camera);
+// 	}
 
-	animate() {
-		this.particleMaterial.uniforms.uTime.value += 0.02;
-		requestAnimationFrame(this.animate);
-		this.render();
-	}
+// 	animate() {
+// 		this.particleMaterial.uniforms.uTime.value += 0.02;
+// 		requestAnimationFrame(this.animate);
+// 		this.render();
+// 	}
 
-	onMouseMove(e) {
-		this.config.mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
-		this.config.mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
-	}
+// 	onMouseMove(e) {
+// 		this.config.mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
+// 		this.config.mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
+// 	}
 
-	onResize() {
-		this.config.winWidth = window.innerWidth;
-		this.config.winHeight = window.innerHeight;
-		this.camera.aspect = this.config.winWidth / this.config.winHeight;
-		this.camera.updateProjectionMatrix();
-		this.renderer.setSize(this.config.winWidth, this.config.winHeight);
-	}
-}
+// 	onResize() {
+// 		this.config.winWidth = window.innerWidth;
+// 		this.config.winHeight = window.innerHeight;
+// 		this.camera.aspect = this.config.winWidth / this.config.winHeight;
+// 		this.camera.updateProjectionMatrix();
+// 		this.renderer.setSize(this.config.winWidth, this.config.winHeight);
+// 	}
+// }
 
-new Canvas();
+// new Canvas();
